7cd8c4b92be3aab6d6425d316e80c454f8a63bc6
karishmathool <nthool005@gmail.com>
2023-04-09 21:23:54 +0530

    added

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+
+from flask import Flask,request,app,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    scaler.transform(np.array(data).reshape(1,-1))
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
+    
+    
+if __name__=="__main__":
+    app.run(debug=True)
